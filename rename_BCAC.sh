0c2b50d517c2b1d1a7b3a369f3c6fba66231fdb9
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2025-01-18 12:34:18 +0000

    Update rename_BCAC.sh

--- rename_BCAC.sh
+++ rename_BCAC.sh
@@ -8,15 +8,32 @@ awk -v OFS='\t' -v cols="$columns" '
     BEGIN {
         n = split(cols, col_array, " ")
         for (i=1; i<=n; i++) {
-            col_index[col_array[i]] = i
+            col_index[col_array[i]] = i 
         }
     }
-    {
+    NR==1 { 
+        for (i=1; i<=NF; i++) {
+            header[i] = $i 
+        }
         for (i=1; i<=n; i++) {
-            printf "%s\t", $col_index[col_array[i]]
+            for (j=1; j<=NF; j++) {
+                if (header[j] == col_array[i]) {
+                    printf "%s\t", header[j] 
+                    break 
+                }
+            }
+        }
+        printf "\n" 
+    }
+    NR>1 { 
+        for (i=1; i<=n; i++) {
+            for (j=1; j<=NF; j++) {
+                if (header[j] == col_array[i]) {
+                    printf "%s\t", $j 
+                    break 
+                }
+            }
         }
         printf "\n"
     }
 ' "$input_file" > "$output_file"
-
-echo "Extracted columns saved to: $output_file"
